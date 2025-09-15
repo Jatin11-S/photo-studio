50995f7caf7ce2bbf42c37ea49be6d6d48b494e8
Jatin Sachdeva <jsjatin11@gmail.com>
2025-09-15 22:04:25 +0530

    Add initial implementation of generative_fill and lifestyle_shot_by_text functions

--- services/generative_fill.py
+++ services/generative_fill.py
@@ -1,2 +1,15 @@
-def generative_fill():
+def generative_fill(
+    api_key: str,
+    image_data: bytes,
+    mask_data: bytes,
+    prompt: str,
+    negative_prompt: Optional[str] = None,
+    num_results: int = 4,
+    sync: bool = False,
+    seed: Optional[int] = None,
+    content_moderation: bool = False,
+    mask_type: str = "manual"
+    ) -> Dict[str, Any]:
+    ''' It enables the generation of objects by prompt in a specific mask area'''
+
     pass
